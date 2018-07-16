ceebf55ea2c24985f10ca309668cc20a9258633e
Nelson Sousa <nelson.sousa@alert-online.com>
2018-07-16 16:07:25 +0100

    Buttons function in controller to apply style

--- app/src/layout/buttons/buttons.component.js
+++ app/src/layout/buttons/buttons.component.js
@@ -12,6 +12,17 @@
     
     function ButtonsController() {
       var $ctrl = this;
+
+      $ctrl.getButtonsStyle = function(path) {
+
+        var re = new RegExp('([0-9]\.){2,}');
+
+        if (re.test(path)) {
+          return 'buttons1';
+        } else {
+           return 'buttons';
+        }
+      }
     }    
 
 })();
